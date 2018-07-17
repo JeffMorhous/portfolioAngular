f30c6f9e9f5ae394688f36da4962262162bb23bd
Jeff Morhous <jobboy18@gmail.com>
2018-07-17 16:42:29 -0400

    Continue projects work

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -7,10 +7,15 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
   cards = [
-    { title: 'Stock Data Retrieval', cols: 1, rows: 1, image: '../assets/installing.png', date: 'July 2018' },
-    { title: 'Angular Personal Portfolio', cols: 1, rows: 1, image: '../assets/angularPortfolio.png', date: 'July 2018' },
-    { title: 'Fundamentals of Engineering Advanced Energy Vehicle', cols: 1, rows: 1, image: '../assets/AEV.jpg', date: 'Spring 2018' },
-    { title: 'Static Personal Portfolio Site', cols: 1, rows: 1, image: '../assets/staticPortfolio.png', date: 'December 2017' },
-    { title: 'Fundamentals of Engineering Software Design Project', cols: 1, rows: 1, image: '../assets/presentation.jpg', date: 'November 2017' }
+    { title: 'Stock Data Retrieval', cols: 1, rows: 1, image: '../assets/installing.png', date: 'July 2018',
+      description: 'Uses AlphaVantage web API to fetch real-time metadata about the stock market.  Expanding to create UI for searching symbols and visualizing prices.' },
+    { title: 'Angular Personal Portfolio', cols: 1, rows: 1, image: '../assets/angularPortfolio.png', date: 'July 2018',
+      description: '' },
+    { title: 'Fundamentals of Engineering Advanced Energy Vehicle', cols: 1, rows: 1, image: '../assets/AEV.jpg',
+     date: 'Spring 2018', description: '' },
+    { title: 'Static Personal Portfolio Site', cols: 1, rows: 1, image: '../assets/staticPortfolio.png', 
+      date: 'December 2017', description: '' },
+    { title: 'Fundamentals of Engineering Software Design Project', cols: 1, rows: 1, image: '../assets/presentation.jpg',
+     date: 'November 2017',description:'' }
   ];
 }
